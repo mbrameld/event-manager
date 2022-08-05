d29f81b7918b79e0d0c955eb4e8f48268070ff0c
Mike Brameld <mbrameld@gmail.com>
2022-08-04 20:37:35 -0700

    Refactor env

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,7 +4,7 @@ import EmailProvider, { EmailConfig } from "next-auth/providers/email";
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { prisma } from "../../../server/db/client";
-import { env } from "../../../server/env.mjs";
+import { env } from "../../../env/server.mjs";
 import { theme } from "../../../theme";
 
 const THIRTY_DAYS = 30 * 24 * 60 * 60;
--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,26 +1,15 @@
 // src/server/router/index.ts
-import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { authRouter } from "./subroutes/auth";
 import { ambassadorRouter } from "./subroutes/ambassador";
 import { eventRouter } from "./subroutes/event";
 import { eventTypeRouter } from "./subroutes/event-type";
 import { dispensaryUserRouter } from "./subroutes/dispensary-user";
-import { TRPCError } from "@trpc/server";
+import { createProtectedRouter } from "./protected-router";
 
-export const appRouter = createRouter()
+export const appRouter = createProtectedRouter()
   .transformer(superjson)
-  .middleware(async ({ ctx, next }) => {
-    // Any queries or mutations after this middleware will
-    // raise an error unless there is a current session
-    if (!ctx.session) {
-      throw new TRPCError({ code: "UNAUTHORIZED" });
-    }
-    return next();
-  })
   .merge("ambassador.", ambassadorRouter)
-  .merge("auth.", authRouter)
   .merge("dispensary.", dispensaryUserRouter)
   .merge("event.", eventRouter)
   .merge("event-type.", eventTypeRouter);
